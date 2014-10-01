7ea0a29007b7bbf37cae11921231c8183d01fb87
Jerry Greenberg <jpg@gcn-20-88.sdsc.edu>
2014-10-01 13:46:22 -0700

    fixed PKGROOT path

--- src/mrbayes/version.mk
+++ src/mrbayes/version.mk
@@ -1,6 +1,5 @@
 REVISION           = r.879
 VERSION            = 3.2.2.$(REVISION)
-PKGROOT            = /opt/mrbayes/$(VERSION)
 NAME               = mrbayes
 RELEASE            = 4
 TARBALL_POSTFIX    = tar.gz
@@ -12,6 +11,7 @@ SOURCE_VERSION     = 3.2.2
 SOURCE_SUFFIX      = tar.gz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR         = $(SOURCE_NAME)_$(SOURCE_VERSION)
+PKGROOT            = /opt/mrbayes/$(SOURCE_VERSION)
 
 REVISION_NAME      = mrbayes-code-879-trunk
 REVISION_SUFFIX    = zip
