ae0b394a485991f53d1281aea6aaadad6a4dfd61
Jerry Greenberg <jpg@gcn-20-88.sdsc.edu>
2015-02-11 14:20:32 -0800

    added version 3.2.4

--- src/mrbayes-3.2.3/version.mk
+++ src/mrbayes-3.2.3/version.mk
@@ -1,12 +1,12 @@
-VERSION            = 3.2.4
-NAME               = sdsc-mrbayes
+VERSION            = 3.2.3
+NAME               = sdsc-mrbayes-$(VERSION)
 RELEASE            = 0
 TARBALL_POSTFIX    = tar.gz
 
 SRC_SUBDIR         = mrbayes
 
 SOURCE_NAME        = mrbayes
-SOURCE_VERSION     = $(VERSION)
+SOURCE_VERSION     = 3.2.3
 SOURCE_SUFFIX      = tar.gz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
