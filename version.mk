3dd7f35ed26646bd2293afd95807dabf25c88efc
Jerry Greenberg <jpg@comet-devel.local>
2015-03-10 00:30:41 -0700

    added commit label to roll name

--- version.mk
+++ version.mk
@@ -1,6 +1,6 @@
-ROLLNAME	= mrbayes
-VERSION		= 1.0
-RELEASE		= 8
-COLOR		= firebrick
+ROLLNAME        = mrbayes
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
+COLOR           = orange
 
-REDHAT.ROOT	= $(CURDIR)
+REDHAT.ROOT     = $(PWD)
