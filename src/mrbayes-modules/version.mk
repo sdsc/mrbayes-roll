dd9c53c97d342ce7ef3dca1ec478edcfe9675a1b
Jerry Greenberg <jpg@comet-devel.sdsc.edu>
2015-05-19 11:10:01 -0700

    added a new target for different mpi's in module Makefile

--- src/mrbayes-modules/version.mk
+++ src/mrbayes-modules/version.mk
@@ -7,6 +7,7 @@ PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
 MAKEFILE_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/Makefile
+EXTRA_MAKEFILE_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)-$(VERSION)/Makefile
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
