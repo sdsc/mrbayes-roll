b38c6a93b60e3429b5db39f2356b0bcd3bf3ccb2
Jerry Greenberg <jpg@gcn-20-88.sdsc.edu>
2015-05-20 21:47:38 -0700

    added patch files

--- src/mrbayes/version.mk
+++ src/mrbayes/version.mk
@@ -12,7 +12,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 VERSION            = 3.2.5
 NAME               = sdsc-mrbayes-$(VERSION)
-RELEASE            = 0
+RELEASE            = 1
 TARBALL_POSTFIX    = tar.gz
 
 SRC_SUBDIR         = mrbayes
@@ -22,8 +22,15 @@ SOURCE_VERSION     = 3.2.5
 SOURCE_SUFFIX      = tar.gz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+UPDATE_NAME        = mrbayes
+UPDATE_VERSION     = 3.2.5-1040
+UPDATE_SUFFIX      = tar.gz
+UPDATE_PKG         = $(UPDATE_NAME)-$(UPDATE_VERSION).$(UPDATE_SUFFIX)
+UPDATE_DIR         = $(UPDATE_PKG:%.$(UPDATE_SUFFIX)=%)
+
 PKGROOT            = /opt/mrbayes/$(VERSION)
 
-TAR_GZ_PKGS        = $(SOURCE_PKG)
+TAR_GZ_PKGS        = $(SOURCE_PKG) $(UPDATE_PKG)
 
 RPM.EXTRAS     = AutoReq:No
