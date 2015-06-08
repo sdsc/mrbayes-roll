f3caf9c90a7c92f070bf927dcc030c60e4a6b23c
Jerry Greenberg <jpg@gcn-20-88.sdsc.edu>
2015-06-08 08:47:41 -0700

    got rid of v3.2.4 module file

--- src/mrbayes-modules/version.mk
+++ src/mrbayes-modules/version.mk
@@ -2,7 +2,7 @@ PACKAGE     = mrbayes
 CATEGORY    = applications
 
 NAME        = sdsc-$(PACKAGE)-modules
-RELEASE     = 4
+RELEASE     = 5
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
@@ -11,7 +11,7 @@ EXTRA_MAKEFILE_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)-$(VERSION)/Makefile
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
-EXTRA_MODULE_VERSIONS = 3.2.3 3.2.4
+EXTRA_MODULE_VERSIONS = 3.2.3
 
 RPM.EXTRAS  = AutoReq:No
 
