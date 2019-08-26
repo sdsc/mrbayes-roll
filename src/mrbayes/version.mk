2005bedbaa885f49a5b147686978d61793bbe6c9
Jerry Greenberg <jpg@sdsc.edu>
2019-08-25 17:19:03 -0700

    APS-1028 mrbayes 3.2.7a

--- src/mrbayes/version.mk
+++ src/mrbayes/version.mk
@@ -9,15 +9,15 @@ endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-mrbayes-$(VERSION)
-VERSION        = 3.2.6
-RELEASE        = 5
+VERSION        = 3.2.7a
+RELEASE        = 0
 PKGROOT        = /opt/mrbayes/$(VERSION)
 
 SRC_SUBDIR     = mrbayes
 
-SOURCE_NAME    = mrbayes
+SOURCE_NAME    = MrBayes
 SOURCE_SUFFIX  = tar.gz
-SOURCE_VERSION = 3.2.5
+SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
